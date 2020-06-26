8d7e81d7cf093d5e329e37ccb52d2868447fb3ea
DangQuy <ngoc_quy@allgrow-labo.jp>
2020-06-26 17:17:32 +0700

    commut

---
+++ 05_change_color_action/event.js
@@ -0,0 +1,7 @@
+'use strict';
+
+chrome.browserAction.onClicked.addListener(function() {
+    chrome.storage.sync.get({color: 'red'}, function(item) {
+        chrome.tabs.executeScript({code: 'document.body.style.backgroundColor = "' + item.color + '"'});
+    })
+});

4a76d2db05ec7ba415638a9e3820feae0f0052c1
DangQuy <ngoc_quy@allgrow-labo.jp>
2020-06-26 13:06:11 +0700

    commut

--- 04_chnage_color/popup.js
+++ 04_chnage_color/popup.js
@@ -1,12 +1,5 @@
 'use strict';
 
-chrome.tabs.query({active: true, lastFocusedWindow:true}, function(tabs) {
-    var i;
-    var title = [];
-    var results = document.getElementById('result');
-    for(i = 0; i <tabs.length; i++) {
-        title.push(tabs[i].title);
-    }
-    results.value = title.join("\n");
-    results.select();
+document.getElementById('colors').addEventListener('change', function(tabs) {
+    chrome.tabs.executeScript({code: 'document.body.style.backgroundColor = "' + this.value + '"'});
 });
